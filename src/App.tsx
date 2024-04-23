f14f77248a22a8067786d8950a91a3edda74969f
Chrishan <chrishan@palicos-mbp-3.home>
2024-04-23 15:18:26 +0200

    Feature : input number

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+import { NumericFormat } from "react-number-format";
 import styled from "styled-components";
-import TitlePage from "./conponents/TitlePage";
+import TitlePage from "./components/TitlePage";
 
 export default function App() {
+  const [amount, setAmount] = useState<number | string>("");
+
+  const handleChange = (values: any) => {
+    const { value } = values;
+    setAmount(value);
+  };
+
   return (
     <Container>
       <TitlePage title={"On : develop"} />
+      <TitlePage title={"input test : "} />
+
+      <ContainerInput>
+        <p>Amount</p>
+        <NumericFormat
+          thousandSeparator={true}
+          suffix="$"
+          allowNegative={false}
+          decimalScale={2}
+          fixedDecimalScale={true}
+          value={amount}
+          onValueChange={(values) => handleChange(values)}
+        />
+      </ContainerInput>
+      {amount !== "" && <p>{amount} $</p>}
     </Container>
   );
 }
@@ -13,6 +37,22 @@ const Container = styled.div`
   min-height: 100vh;
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  gap: 20px;
   align-items: center;
   font-family: "Poppins", Courier, monospace;
+`;
+
+const ContainerInput = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  p {
+    font-size: 20px;
+  }
+  input {
+    padding: 10px;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
 `;
